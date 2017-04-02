764154af96fe125e997f0cdfec65ebb024110a4e
DESKTOP-HOME\Charles <cdgiessen@gmail.com>
2017-04-01 21:47:24 -0500

    Put stb_image loader into a library directory (outside of the repo essentially)

--- OrangeSherbetGameEngine/OrangeSherbetGameEngine.cpp
+++ OrangeSherbetGameEngine/OrangeSherbetGameEngine.cpp
@@ -7,7 +7,7 @@
 //#include <glm/gtc/type_ptr.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <stb_image.h>
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
